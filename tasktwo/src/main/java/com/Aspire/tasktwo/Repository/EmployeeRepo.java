cdeea9367e7c0c7959721d1e0a3a46ea061246ec
PoojaNairP <84313301+PoojaNairP@users.noreply.github.com>
2024-08-20 11:10:17 +0530

    Add files via upload

---
+++ tasktwo/src/main/java/com/Aspire/tasktwo/Repository/EmployeeRepo.java
@@ -0,0 +1,60 @@
+package com.Aspire.tasktwo.Repository;
+
+//import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+//import java.util.Map;
+//import java.util.Set;
+//import java.util.Optional;
+import java.util.Optional;
+
+//import java.util.List;
+
+import com.Aspire.tasktwo.Model.Employee;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+//import org.springframework.data.repository.query.Param;
+
+public interface EmployeeRepo extends MongoRepository<Employee,Integer> {
+
+    boolean existsById(Integer employeeId);
+    boolean existsByDepartmentAndDesignation(String department, String string);
+    @Query(value = "{'designation': ?0, 'department': ?1}")
+    Optional<Employee> findEmployeeByDesignationAndDepartment(String designation, String department);
+
+    // Find all employee IDs
+    @Query(value = "{}", fields = "{ 'employeeId' : 1 }")
+    List<Integer> findAllEmployeeIds();
+
+    // Find the manager for each department
+    @Query(value = "{ 'designation': 'Manager' }", fields = "{ 'department' : 1, 'employeeId': 1 }")
+    List<Employee> findDepartmentManagers();
+
+    
+
+    List<Employee> findEmployeesByManagerId(Integer managerId);
+
+    List<Employee> findByManagerId(Integer managerId);
+
+    // Find an employee by designation and department
+    
+
+    // Find employees by experience cutoff date and managerId
+    @Query(value = "{'dateOfJoining': { $lte: ?0 }, 'managerId': ?1}")
+    List<Employee> findByExperienceAndManagerId(LocalDateTime experienceCutoffDate, int managerId);
+
+    // Find employees by experience cutoff date
+    @Query(value = "{'dateOfJoining': { $lte: ?0 }}")
+    List<Employee> findByExperience(LocalDateTime experienceCutoffDate);
+
+    Optional<Employee> findByEmployeeId(int employeeId);
+
+    //Optional<Integer> findEmployeeIdByDesignationAndDepartment(String string, String department);
+
+    //List<Employee> findByManagerId(Integer managerId);
+
+    
+
+    
+}
